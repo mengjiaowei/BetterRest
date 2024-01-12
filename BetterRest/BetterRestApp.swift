261085f1403b33947d1379d13600bd8d7f719a31
mengjiaowei <mengjiao.wei@icloud.com>
2024-01-13 09:24:26 +1100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by MJ Wei on 13/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
